4b33cf21750b3821b80ad53b1f73f0b0a2f95a1d
arthuurtm <146558116+arthuurtm@users.noreply.github.com>
2024-11-02 01:35:44 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <Navigator 
+      :profilePicture="userProfilePicture" 
+      :name="userName" 
+      :email="userEmail"
+      @navigate="handleNavigation"
+      @logout="handleLogout"
+    />
+    <!-- <router-view /> -->
+  </div>
+</template>
+
+<script>
+import Navigator from "./components/MainNavigator.vue";
+
+export default {
+  name: 'app',
+  components: {
+    Navigator
+  },
+  data() {
+    return {
+      userProfilePicture: "caminho_da_imagem_perfil",
+      userName: "Nome do Usuário",
+      userEmail: "email@exemplo.com"
+    };
+  },
+  methods: {
+    handleNavigation(section) {components
+      // Lógica para navegação
+      this.$router.push({ name: section });
+    },
+    handleLogout() {
+      // Lógica para logout
+      console.log("Logout solicitado");
+    }
+  }
+};
+</script>
+
+<style>
+    @import url('/src/assets/css/core/variables.css');
+    @import url('/src/assets/css/core/buttons.css');
+    @import url('/src/assets/css/core/overlay.css');
+</style>
