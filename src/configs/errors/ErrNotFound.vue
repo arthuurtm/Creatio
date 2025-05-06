4eb9b327c68a4f6507530006c04f0d375f750ad6
Tui <146558116+arthuurtm@users.noreply.github.com>
2025-05-05 22:37:17 -0300

    melhorias na interface de erro 404

--- src/configs/errors/ErrNotFound.vue
+++ src/configs/errors/ErrNotFound.vue
@@ -24,28 +24,94 @@ export default {
   data() {
     return {
       msgText: window.location.pathname,
-      maxLength: 20
+      maxLength: 20,
     }
   },
   mounted() {
-    const truncatedText = this.truncateText(this.msgText, this.maxLength);
-    document.getElementById("message-text").textContent = truncatedText;
+    const truncatedText = this.truncateText(this.msgText, this.maxLength)
+    document.getElementById('message-text').textContent = truncatedText
   },
   methods: {
     truncateText(text, maxLength) {
       if (text.length > maxLength) {
-        return text.substring(0, maxLength - 3) + "...";
+        return text.substring(0, maxLength - 3) + '...'
       } else {
-        return text;
+        return text
       }
     },
     back() {
-      this.$globalFunc.hrefTo('/');
+      this.$globalFunc.hrefTo('/')
     },
-  }
+  },
 }
 </script>
 
 <style scoped>
-@import url('/src/assets/css/layouts/l-errPage.css');
+main {
+  max-width: 1600px;
+  max-height: 800px;
+}
+
+.separador {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 20px;
+  padding: 20px;
+}
+
+.container {
+  flex: 1;
+  box-sizing: border-box;
+  margin: 30px;
+}
+
+.text-container {
+  text-align: left;
+}
+
+.text-container h2 {
+  font-weight: normal;
+}
+
+.image-container {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.image {
+  max-width: 80%;
+  height: auto;
+  width: 500px;
+  animation: rotateImage 10s infinite linear;
+  transform-style: preserve-3d;
+}
+
+.btn {
+  max-width: 50%;
+}
+
+@keyframes rotateImage {
+  from {
+    transform: rotateY(0deg); /* Começa a rotação em 0 graus */
+  }
+  to {
+    transform: rotateY(360deg); /* Completa uma volta em 360 graus */
+  }
+}
+
+@media (max-width: 800px) {
+  .separador {
+    display: block;
+  }
+
+  .image {
+    max-width: 100%;
+    width: 100%;
+  }
+
+  .container {
+    margin: 0px 0px 20px 0px;
+  }
+}
 </style>
