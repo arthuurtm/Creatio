30555510ccb81b6063be937c3a09f6fc0bc8012d
Tui <146558116+arthuurtm@users.noreply.github.com>
2025-07-20 14:56:27 -0300

    removendo função descontinuada 'createStores'

--- src/stores/index.js
+++ src/stores/index.js
@@ -3,12 +3,3 @@ import { useFormStore } from './form'
 import { useGlobalStore, useSettingsStore, useAppDynamicDialog } from './global'
 
 export { useUserStore, useGlobalStore, useSettingsStore, useAppDynamicDialog, useFormStore }
-export function createStores() {
-  return {
-    user: useUserStore(),
-    form: useFormStore(),
-    dialog: useAppDynamicDialog(),
-    global: useGlobalStore(),
-    settings: useSettingsStore(),
-  }
-}
