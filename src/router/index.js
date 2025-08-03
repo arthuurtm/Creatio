cfd5ea43202e6f19fffba619a44fec31ca18390c
Tui <146558116+arthuurtm@users.noreply.github.com>
2025-08-03 15:45:58 -0300

    as rotas foram reorganizadas, separando responsabilidades e tipos de páginas.
    as nomenclarutas foram mantidas (exceto Create -> CreateHome)

--- src/router/index.js
+++ src/router/index.js
@@ -5,116 +5,139 @@ import { isAuthenticated } from '@/functions/auth'
 // Errors
 import ErrNotFound from '@/layouts/ErrNotFound.vue'
 
-// Forms page
-import Login from '@/views/form/FormLogin.vue'
-import Signup from '@/views/form/FormSignup.vue'
-import PasswordRescue from '@/views/form/FormPasswordRescue.vue'
-
-// Main Pages
+// Pages
+import FormLogin from '@/views/form/FormLogin.vue'
+import FormSignup from '@/views/form/FormSignup.vue'
+import FormPasswordRescue from '@/views/form/FormPasswordRescue.vue'
 import ViewAbout from '@/views/ViewAbout.vue'
 import ViewGameDetails from '@/views/ViewGameDetails.vue'
 import ViewHome from '@/views/ViewHome.vue'
+import ViewUserProfile from '@/views/ViewUserProfile.vue'
+import ViewGameRun from '@/views/ViewGameRun.vue'
+import CreateHome from '@/views/create/CreateHome.vue'
+import CreateGameSettings from '@/views/create/CreateGameSettings.vue'
+import EditGame from '@/views/create/EditGame.vue'
 
 // Layouts
-import AppMain from '@/layouts/AppHome.vue'
-import CreateHome from '@/views/create/CreateHome.vue'
-import EditGame from '@/views/create/EditGame.vue'
-import CreateGameSettings from '@/views/create/CreateGameSettings.vue'
+import AppHome from '@/layouts/AppHome.vue'
+import AppCreateGame from '@/layouts/AppCreateGame.vue'
+import AppGame from '@/layouts/AppGame.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/:pathMatch(.*)*',
-      name: 'ErrNotFound',
-      component: ErrNotFound,
-    },
-
-    {
       path: '/',
-      name: 'Main',
+      name: 'About',
       component: ViewAbout,
     },
 
     {
-      path: '/login',
-      redirect: { name: 'Login' },
-    },
-
-    {
-      path: '/app',
-      component: AppMain,
+      path: '/home',
+      component: AppHome,
       children: [
         {
-          path: 'home',
+          path: '',
           name: 'Home',
           component: ViewHome,
-        },
-
-        {
-          path: 'create',
-          name: 'Create',
-          meta: { requiresAuth: true },
-          component: CreateHome,
-        },
-
-        {
-          path: 'create/init',
-          name: 'CreateGame',
-          meta: { requiresAuth: true },
-          component: CreateGameSettings,
-        },
-
-        {
-          path: 'create/:id',
-          name: 'EditGame',
-          meta: { requiresAuth: true, hiddenNavigator: true },
-          component: EditGame,
-          props: true,
-        },
-
-        {
-          path: 'game/:id',
-          name: 'GameDetails',
-          component: ViewGameDetails,
-          props: true,
         },
       ],
     },
 
     {
-      path: '/account',
+      path: '/auth',
       children: [
         {
           path: 'login',
           name: 'Login',
           meta: { requiresAuth: false },
-          component: Login,
+          component: FormLogin,
         },
         {
           path: 'signup',
           name: 'Signup',
           meta: { requiresAuth: false },
-          component: Signup,
+          component: FormSignup,
         },
         {
           path: 'password/rescue',
           name: 'PasswordRescue',
-          component: PasswordRescue,
+          component: FormPasswordRescue,
         },
       ],
+    },
+
+    {
+      path: '/u/:username',
+      name: 'UserProfile',
+      component: ViewUserProfile,
+      props: true,
+    },
+
+    {
+      path: '/games',
+      component: AppGame,
+      children: [
+        {
+          path: ':id',
+          name: 'GameDetails',
+          component: ViewGameDetails,
+          props: true,
+        },
+        {
+          path: ':id/run',
+          name: 'GameRun',
+          component: ViewGameRun,
+          props: true,
+        },
+      ],
+    },
+
+    {
+      path: '/games/create',
+      component: AppCreateGame,
+      meta: { requiresAuth: true },
+      children: [
+        {
+          path: '',
+          name: 'CreateHome',
+          component: CreateHome,
+        },
+        {
+          path: 'init',
+          name: 'CreateGame',
+          component: CreateGameSettings,
+        },
+        {
+          path: ':id',
+          name: 'EditGame',
+          component: EditGame,
+          props: true,
+          meta: { hiddenNavigator: true },
+        },
+      ],
+    },
+
+    // Redirecionamentos e erros
+    {
+      path: '/login',
+      redirect: { name: 'Login' },
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'ErrNotFound',
+      component: ErrNotFound,
     },
   ],
 })
 
 // Router Guard
 router.beforeEach(async (to, from, next) => {
-  // console.log(to, from, next)
   const isLoggedIn = await isAuthenticated()
 
   if (to.meta.requiresAuth && !isLoggedIn) {
     next({ name: 'Login', query: { redirect: to.name } })
-  } else if ((to.name === 'Login' || to.name === 'Main') && isLoggedIn) {
+  } else if ((to.name === 'Login' || to.name === 'About') && isLoggedIn) {
     next({ name: 'Home' })
   } else {
     next()
