75ac88cf8372d60fe5aecb396c92bb771f3b7939
Tui <146558116+arthuurtm@users.noreply.github.com>
2025-08-10 16:23:03 -0300

    adicionado compatibilidade com posicionamento definido no objeto

--- src/components/elements/CreateButton.vue
+++ src/components/elements/CreateButton.vue
@@ -4,10 +4,15 @@
     <component
       v-if="!button?.rules?.includes('hide')"
       :is="button.tag || 'button'"
-      :class="[!button.tag && 'btn', button.class, button?.position, globalStyle]"
+      :class="[
+        !button.tag && 'btn',
+        button.class,
+        typeof button?.position === 'string' && button.position,
+        globalStyle,
+      ]"
       :id="button.id || ''"
       :type="button.type || 'submit'"
-      :style="button?.style"
+      :style="[button?.style, typeof button?.position === 'object' && button.position]"
       @click="
         (typeof button.action === 'function'
           ? handleAction(button, $event)
