7ed16d8481f6853ceeeef2bebd006eccd5269362
Arthur <arthuurtm@gmail.com>
2025-08-30 17:33:54 -0300

    adicionado controllers para o websocket: atualizar e ler.

---
+++ api/controllers/ws/GameLabController.js
@@ -0,0 +1,31 @@
+import FileService from '../../services/FileService.js'
+
+async function update(ws, data) {
+  const key = `game_${data.gameId}_editor`
+
+  FileService.write
+    .queueSave(key, data.state, {
+      bucket: 'private',
+      meta: { 'Content-Type': 'application/json' },
+      objectNameGenerator: () => `games/${data.gameId}/editor/version-${data.version}.json`,
+    })
+    .then((objectName) => {
+      ws.send(JSON.stringify({ event: 'game:lab:update:success', data: { objectName } }))
+    })
+    .catch((err) => {
+      ws.send(JSON.stringify({ event: 'game:lab:update:error', data: { message: err.message } }))
+    })
+}
+
+async function get(ws, data) {
+  FileService.read
+    .readJson('private', data.fileName)
+    .then((result) => {
+      ws.send(JSON.stringify({ event: 'game:lab:get:success', data: result }))
+    })
+    .catch((err) => {
+      ws.send(JSON.stringify({ event: 'game:lab:get:error', data: { message: err.message } }))
+    })
+}
+
+export default { update, get }
