f7963bdf44396e38df31c80a82b6773d61c17218
Tui <146558116+arthuurtm@users.noreply.github.com>
2025-08-31 14:25:05 -0300

    agora o arquivo de configuração do banco de dados configura ele antes e envia um singleton

--- api/config/db.js
+++ api/config/db.js
@@ -1,18 +1,18 @@
+// Arquivo: database/index.js (ou onde preferir)
+
 import { Sequelize } from 'sequelize'
 import mariadb from 'mariadb'
-import dotenv from 'dotenv'
 import log from '../helpers/console.js'
-dotenv.config({ path: '../../.env' })
 
-let sequelize = null
-try {
-  const pool = mariadb.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  })
-  // cria banco se não existir
-  ;(async () => {
+async function initialize() {
+  try {
+    // garantir que o banco de dados exista
+    const pool = mariadb.createPool({
+      host: process.env.DB_HOST || 'localhost',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    })
+
     let conn
     try {
       conn = await pool.getConnection()
@@ -20,19 +20,35 @@ try {
         `CREATE DATABASE IF NOT EXISTS \`${process.env.DATABASE}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci;`,
       )
       log.info('Database verificado/criado com sucesso!')
-    } catch (error) {
-      log.error('Erro ao verificar/criar o banco de dados:', error)
     } finally {
       if (conn) conn.release()
+      // serviu apenas para criar o DB, fechado
+      await pool.end()
     }
-  })()
 
-  sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mariadb',
-  })
-} catch (error) {
-  log.error('Erro ao carregar banco de dados: ', error)
+    // criar e autenticar a instância do Sequelize
+    const sequelizeInstance = new Sequelize(
+      process.env.DATABASE,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'mariadb',
+        logging: false, // não poluir o console
+      },
+    )
+
+    // teste de conexão
+    await sequelizeInstance.authenticate()
+    log.success('Conexão com o Sequelize estabelecida com sucesso.')
+
+    return sequelizeInstance
+  } catch (error) {
+    log.error('Não foi possível conectar ao banco de dados: ', error)
+    process.exit(1)
+  }
 }
+
+const sequelize = await initialize()
 
 export default sequelize
