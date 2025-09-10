de101f647ba8bb39967b9e7de24d7ab09e3dc0b1
Tui <146558116+arthuurtm@users.noreply.github.com>
2025-09-09 21:22:09 -0300

    feat: adiciona função selectFile para seleção de arquivos com URL temporária

--- src/functions/util.js
+++ src/functions/util.js
@@ -48,4 +48,36 @@ function handleImage(file, isPublic = true) {
   return null
 }
 
-export default { appTheme, handleImage }
+/**
+ *
+ * @param {string} type
+ * @returns {Promise<{file: File, url: string}>}
+ */
+const selectFile = (type) => {
+  return new Promise((resolve) => {
+    const input = document.createElement('input')
+    input.type = 'file'
+    input.accept = type || 'all/*'
+    input.style.display = 'none'
+
+    input.onchange = (event) => {
+      const file = event.target.files[0]
+      if (file) {
+        const url = URL.createObjectURL(file) // gera URL temporária
+        resolve({ file, url })
+      } else {
+        resolve(null)
+      }
+    }
+
+    document.body.appendChild(input)
+    input.click()
+
+    // remove depois
+    input.addEventListener('blur', () => {
+      document.body.removeChild(input)
+    })
+  })
+}
+
+export default { appTheme, handleImage, selectFile }
