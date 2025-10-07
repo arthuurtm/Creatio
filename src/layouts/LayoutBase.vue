8e29dedc0b82c25fcf615fb836894a74867a35b8
Tui <146558116+arthuurtm@users.noreply.github.com>
2025-10-07 14:23:37 -0300

    refactor(ui): reorganiza e otimiza a estrutura do LayoutBase, substitui ComponentNavigator e ajusta estilos

--- src/layouts/LayoutBase.vue
+++ src/layouts/LayoutBase.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { computed, ref } from 'vue'
 import { useRoute } from 'vue-router'
-import ComponentNavigator from '@/components/modules/ComponentNavigator.vue'
+import ComponentNavigator from '@/components/modules/ComponentHeader.vue'
 
 const route = useRoute()
 const pageMeta = computed(() => route?.meta)
@@ -11,47 +11,51 @@ const pageName = computed(() => route?.name)
 
 <template>
   <div class="app-container">
-    <div class="app-content" :class="[pageMeta.hiddenNavigator && 'overlay-nav']">
-      <div class="app-navigator">
-        <ComponentNavigator
-          :hidden="pageMeta.hiddenNavigator"
-          :page="pageName"
-          :defaultHideButton="true"
-          @navigatorStatus="updateNavStatus"
-          ref="navigator"
-        />
-      </div>
-
-      <div class="app-view" :class="[pageMeta.fullscreen && 'full']">
-        <router-view v-slot="{ Component }">
-          <transition name="fastFade" mode="out-in">
-            <div :key="route.path" style="width: 100%; height: 100%">
-              <component :is="Component" />
-            </div>
-          </transition>
-        </router-view>
-      </div>
+    <ComponentNavigator
+      :hidden="pageMeta.hiddenNavigator"
+      :page="pageName"
+      :defaultHideButton="true"
+      @navigatorStatus="updateNavStatus"
+      ref="navigator"
+    />
+    <div
+      class="app-content"
+      :class="[pageMeta.hiddenNavigator && 'overlay-nav', pageMeta.fullscreen && 'full']"
+    >
+      <router-view v-slot="{ Component }">
+        <transition name="fastFade" mode="out-in">
+          <div :key="route.path" style="width: 100%; height: 100%">
+            <component :is="Component" />
+          </div>
+        </transition>
+      </router-view>
     </div>
   </div>
 </template>
 
 <style scoped>
 .app-container {
-  display: grid;
-  grid-template-rows: auto 1fr;
-  grid-template-columns: 1fr;
+  display: flex;
   height: 100%;
   width: 100%;
   position: relative;
+  flex-direction: column;
+}
+
+.app-header {
+  height: auto;
 }
 
 .app-content {
-  display: grid;
-  flex-direction: column;
-  grid-row: 2;
-  height: 100%;
+  display: flex;
   overflow: hidden;
-  grid-template-columns: auto 1fr;
+  padding: 0.5rem;
+  z-index: 1;
+  height: 100%;
+}
+
+.app-content.full {
+  padding: 0;
 }
 
 .app-navigator {
@@ -59,37 +63,12 @@ const pageName = computed(() => route?.name)
   position: sticky;
   grid-column: 1;
   z-index: 2;
-  background: var(--navigator);
-}
-
-.app-view {
-  padding: 1rem 0 1rem 1rem;
-  overflow-y: auto;
-  flex-grow: 1;
-  z-index: 1;
-  grid-column: 2;
-  height: 100%;
-}
-
-.app-view.full {
-  padding: 0;
 }
 
 /* --- MODO MENU ESCONDIDO (QUANDO hidden é ativo) --- */
 .app-content.overlay-nav {
   grid-template-columns: 1fr;
   position: relative;
-}
-
-.app-content.overlay-nav .app-navigator {
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-}
-
-.app-content.overlay-nav .app-view {
-  grid-column: 1;
 }
 
 @media (max-width: 600px) {
