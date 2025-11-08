0bcf9478559d14656b560cb9e74c8b2bcffc6782
Tui <146558116+arthuurtm@users.noreply.github.com>
2025-11-07 21:46:22 -0300

    refactor(CInputSelect): substitui BaseButton por componente dinâmico e melhora a lógica de seleção

--- src/components/ui/CInputSelect.vue
+++ src/components/ui/CInputSelect.vue
@@ -1,27 +1,43 @@
 <template>
-  <BaseButton v-bind="{ ...$props, ...$attrs }" @click="openMenu">
+  <component :is="componentValue" v-bind="{ ...$props, ...$attrs }" @click="openMenu">
     <template v-for="(_, slotName) in $slots" #[slotName]>
       <slot :name="slotName"></slot>
     </template>
-  </BaseButton>
+    <template #default>
+      <b
+        ><span>{{ !onlyIcon ? displayValue : '' }}</span></b
+      >
+    </template>
+    <template #trailing>
+      <CButton icon="arrow_drop_down" classes="symbolic no-padding" @click.stop="openMenu"
+    /></template>
+  </component>
 
-  <CContextMenu ref="contextMenu" :no-floating="noFloating" @select="$emit('select', $event)" />
+  <CContextMenu ref="contextMenu" :no-floating="noFloating" @select="selectHandler" />
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import BaseButton from '@/components/base/BaseButton.vue'
 import { baseInputEmits, baseInputProps } from '@/components/base/BaseInput.props'
+import BaseInput from '../base/BaseInput.vue'
+import BaseButton from '../base/BaseButton.vue'
 const contextMenu = ref(null)
 const props = defineProps({
   ...baseInputProps,
   options: { type: Array, default: () => [] },
   noFloating: Boolean,
+  onlyIcon: Boolean,
 })
-defineEmits([...baseInputEmits, 'select'])
+const componentValue = props.onlyIcon ? BaseButton : BaseInput
+const displayValue = ref('')
+const emit = defineEmits([...baseInputEmits, 'select'])
 defineOptions({ inheritAttrs: false })
 function openMenu(event) {
   if (!props.options?.length) return
   contextMenu.value?.openContextMenu(props.options, event)
+}
+function selectHandler(e) {
+  displayValue.value = e.text || e.label || ''
+  emit('select', e)
 }
 </script>
