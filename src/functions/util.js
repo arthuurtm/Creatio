d7e0de72a132bae7a30c301c5fd9fc01135f7a02
Tui <146558116+arthuurtm@users.noreply.github.com>
2025-12-19 14:04:43 -0300

    feat: remove função appTheme e ajuste na persistência do tema escuro no store de configurações

--- src/functions/util.js
+++ src/functions/util.js
@@ -1,36 +1,3 @@
-function appTheme(toggle = false, glassy = false) {
-  const savedTheme = localStorage.getItem('data-theme')
-  let themeModifier = localStorage.getItem('data-modifier')
-  let currentTheme
-
-  if (savedTheme) {
-    currentTheme = savedTheme
-  } else {
-    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-    currentTheme = prefersDark ? 'dark' : 'light'
-    localStorage.setItem('data-theme', currentTheme)
-  }
-
-  if (glassy) {
-    themeModifier = themeModifier === 'glass' ? 'default' : 'glass'
-    localStorage.setItem('data-modifier', themeModifier)
-  }
-
-  if (toggle) {
-    currentTheme = currentTheme === 'dark' ? 'light' : 'dark'
-    localStorage.setItem('data-theme', currentTheme)
-  }
-
-  document.documentElement.setAttribute('data-theme', currentTheme)
-  document.documentElement.setAttribute('data-modifier', themeModifier || 'default')
-
-  return {
-    currentTheme: currentTheme,
-    isDark: currentTheme === 'dark' ? true : false,
-    isGlassy: themeModifier === 'glass' ? true : false,
-  }
-}
-
 function handleImage(file, isPublic = true) {
   if (file instanceof File || file instanceof Blob) {
     return URL.createObjectURL(file)
@@ -96,4 +63,4 @@ function deepFindById(data, id) {
   return null
 }
 
-export default { appTheme, handleImage, selectFile, deepFindById }
+export default { handleImage, selectFile, deepFindById }
--- src/stores/global.js
+++ src/stores/global.js
@@ -1,16 +1,15 @@
 import { defineStore } from 'pinia'
-import { markRaw } from 'vue'
 
 export const useSettingsStore = defineStore('settings', {
   state: () => ({
-    sideBar: true,
+    darkTheme: false,
   }),
   getters: {
-    getSideBar: (state) => state.sideBar,
+    getDarkTheme: (state) => state.darkTheme,
   },
   actions: {
-    setSideBar(value = false) {
-      this.sideBar = value
+    setDarkTheme(value = false) {
+      this.darkTheme = value
     },
   },
   persist: {
@@ -19,71 +18,8 @@ export const useSettingsStore = defineStore('settings', {
       {
         key: 'settings',
         storage: localStorage,
-        paths: ['settings'],
+        paths: ['darkTheme'],
       },
     ],
-  },
-})
-
-export const useAppDynamicDialog = defineStore('appDynamicDialog', {
-  state: () => ({
-    isVisible: false,
-    component: null,
-    title: 'Diálogo',
-    data: {},
-    history: [],
-  }),
-  getters: {
-    getIsVisible: (state) => state.isVisible,
-    getComponent: (state) => state.component,
-    getTitle: (state) => state.title,
-    getData: (state) => state.data,
-    getIsHistory: (state) => state.history.length > 0,
-  },
-  actions: {
-    show() {
-      this.isVisible = true
-    },
-
-    close() {
-      console.log('close', '; history?', this.history.length)
-      if (this.history.length > 0) {
-        const lastDialog = this.history.pop()
-        this.component = lastDialog.component
-        this.data = lastDialog.data
-        this.title = lastDialog.data.title || 'Diálogo'
-      } else {
-        this.isVisible = false
-        this.component = ''
-        this.data = {}
-      }
-    },
-
-    setDialog(component, data = {}) {
-      console.log('setDialog', component, data)
-      const isFirst = !this.component
-
-      if (!isFirst) {
-        this.history.push({
-          component: this.component,
-          data: this.data,
-        })
-      }
-
-      if (data) {
-        this.data = data
-        this.title = data.title
-      }
-
-      this.component = markRaw(component)
-
-      if (isFirst) {
-        this.show()
-      }
-    },
-
-    setData(data) {
-      this.data = data
-    },
   },
 })
