20e06bf8755d5066ac2818320a68ba13fd7f4ed7
Tui <146558116+arthuurtm@users.noreply.github.com>
2025-12-26 18:26:49 -0300

    refactor: migra o serviço de email para typescript. agora ele retorna um objeto com isAuth e url, também consegue salvar uma sessão já autenticada e restaurar a sessão automaticamente sem precisar fazer login toda vez

--- api/controllers/http/EmailController.ts
+++ api/controllers/http/EmailController.ts
@@ -1,15 +1,14 @@
 import type { Request, Response } from "express";
 import log from "#api/helpers/console.ts";
-
-// import {
-// 	authenticateService,
-// 	sendEmailService,
-// } from "#api/services/EmailService.js";
+import {
+	authenticateService,
+	sendEmailService,
+} from "#api/services/EmailService.ts";
 
 async function sendEmail(req: Request): Promise<boolean> {
 	try {
 		const { template, to, subject } = req.body;
-		// await sendEmailService({ template, to, subject, ...req.body });
+		await sendEmailService({ template, to, subject, ...req.body });
 		return true;
 	} catch (err) {
 		log.error("Erro ao enviar e-mail:", err);
@@ -19,8 +18,8 @@ async function sendEmail(req: Request): Promise<boolean> {
 
 async function authenticate(res: Response) {
 	try {
-		// const url = await authenticateService();
-		// res.send(`Abra este URL: ${url}`);
+		const url = await authenticateService();
+		res.send(`Abra este URL: ${url}`);
 		return true;
 	} catch (err) {
 		res.status(500).json({ message: err });
---
+++ api/services/EmailService.ts
@@ -0,0 +1,158 @@
+import fs from "node:fs";
+import path from "node:path";
+import { CodeChallengeMethod } from "google-auth-library";
+import { google } from "googleapis";
+import nodemailer from "nodemailer";
+import open from "open";
+import log from "../helpers/console.ts";
+
+const API_ROOT = path.join(process.cwd(), "api");
+const API_CONFIG = path.join(API_ROOT, "config");
+const API_TEMPLATES = path.join(API_ROOT, "templates");
+const API_MAIL_CREDENTIAL = path.join(API_CONFIG, "credentials.json");
+const API_LOCAL_MAIL_CREDENTIAL = path.join(
+	API_CONFIG,
+	"credentials.local.json",
+);
+let accessToken: string;
+let refreshToken: string;
+let codeVerifier: string;
+
+const SCOPES = ["https://mail.google.com/"];
+const credentials = JSON.parse(fs.readFileSync(API_MAIL_CREDENTIAL, "utf8"));
+const { client_secret, client_id, redirect_uris } = credentials.web;
+const oAuth2Client = new google.auth.OAuth2(
+	client_id,
+	client_secret,
+	redirect_uris[0],
+);
+
+function loadSavedCredentials() {
+	if (fs.existsSync(API_LOCAL_MAIL_CREDENTIAL)) {
+		const tokenData = fs.readFileSync(API_LOCAL_MAIL_CREDENTIAL, "utf8");
+		const tokens = JSON.parse(tokenData);
+		oAuth2Client.setCredentials(tokens);
+		oAuth2Client.on("tokens", (newTokens) => {
+			const updated = { ...tokens, ...newTokens };
+			fs.writeFileSync(API_LOCAL_MAIL_CREDENTIAL, JSON.stringify(updated));
+		});
+		return true;
+	}
+	return false;
+}
+
+// autenticar o serviço automaticamente
+async function authenticateService() {
+	if (!loadSavedCredentials()) {
+		const { codeVerifier: cv, codeChallenge } =
+			await oAuth2Client.generateCodeVerifierAsync();
+		codeVerifier = cv;
+
+		const url = oAuth2Client.generateAuthUrl({
+			access_type: "offline",
+			scope: SCOPES,
+			prompt: "consent",
+			code_challenge: codeChallenge,
+			code_challenge_method: CodeChallengeMethod.S256,
+		});
+
+		await open(url);
+		return {
+			isAuth: false,
+			url,
+		};
+	}
+	log.success("Serviço de email já autenticado!");
+	return {
+		isAuth: true,
+		url: "",
+	};
+}
+
+// Passo 2 – trocar o "code" pelo token
+async function handleOAuthCallback(code: string) {
+	const { tokens } = await oAuth2Client.getToken({
+		code,
+		codeVerifier,
+		redirect_uri: redirect_uris[0],
+	});
+	oAuth2Client.setCredentials(tokens);
+	fs.writeFileSync(API_LOCAL_MAIL_CREDENTIAL, JSON.stringify(tokens));
+
+	if (tokens.access_token) {
+		accessToken = tokens.access_token;
+		refreshToken = tokens.refresh_token || refreshToken;
+
+		log.success("Credenciais do serviço de email salvas com sucesso!");
+		return tokens;
+	} else {
+		throw new Error("Ocorreu um erro interno no servidor de email.");
+	}
+}
+
+interface EmailParams {
+	template: string;
+	to: string;
+	subject: string;
+	[key: string]: any;
+}
+
+// Passo 3 – enviar email
+async function sendEmailService({
+	template,
+	to,
+	subject,
+	...templateData
+}: EmailParams) {
+	if (!accessToken) throw new Error();
+
+	const transporter = nodemailer.createTransport({
+		service: "gmail",
+		auth: {
+			type: "OAuth2",
+			user: process.env.EMAIL_FROM,
+			clientId: client_id,
+			clientSecret: client_secret,
+			refreshToken,
+			accessToken,
+		},
+	});
+
+	const result = await transporter.sendMail({
+		from: process.env.EMAIL_FROM,
+		to,
+		subject,
+		html: loadTemplate(template, templateData),
+		attachments: [
+			{
+				filename: "bitmap.png",
+				path: path.join(API_TEMPLATES, "bitmap.png"),
+				cid: "unique@cid",
+			},
+		],
+	});
+
+	return { success: true, message: "E-mail enviado", result };
+}
+
+function loadTemplate(name: string, variables: Record<string, any> = {}) {
+	const filePath = path.join(API_TEMPLATES, `${name}.html`);
+	if (!fs.existsSync(filePath)) {
+		log.error("Caminho inválido: ", filePath);
+		throw new Error();
+	}
+
+	let html = fs.readFileSync(filePath, "utf8");
+	for (const key in variables) {
+		html = html.replace(new RegExp(`{{\\s*${key}\\s*}}`, "g"), variables[key]);
+	}
+
+	return html;
+}
+
+export {
+	loadSavedCredentials,
+	authenticateService,
+	handleOAuthCallback,
+	sendEmailService,
+};
