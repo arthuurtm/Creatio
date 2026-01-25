b4c74ffed03db3234fa26f411171a322c925176d
Tui <146558116+arthuurtm@users.noreply.github.com>
2026-01-25 12:33:48 -0300

    feat: adiciona componente IconRender para renderização de ícones com suporte a Lottie, URLs e emojis e simplifica a estrutura do botao customizado

--- src/components/ui/CButton.vue
+++ src/components/ui/CButton.vue
@@ -1,12 +1,62 @@
 <template>
-  <BaseButton v-bind="{ ...$props, ...attrs }" />
+  <v-btn v-bind="attrs" :icon="isIconOnly" :rounded="isIconOnly ? 'circle' : attrs?.rounded" @click="handleClick"
+    @mouseenter="playLottieAnimation" @mouseleave="pauseLottieAnimation">
+
+    <!-- caso icon + texto -->
+    <template v-if="icon && hasContent" #prepend>
+      <IconRenderer ref="iconRef" :icon="icon" />
+    </template>
+
+    <!-- caso icon-only -->
+    <template v-if="icon">
+      <IconRenderer ref="iconRef" :icon="icon" />
+    </template>
+
+    <img v-if="img" :src="img.src" :class="img.class" :style="img.style" />
+
+    <template v-if="text && !hasDefaultSlot">
+      {{ text || label }}
+    </template>
+
+    <slot v-else />
+  </v-btn>
 </template>
 
 <script setup lang="ts">
-import BaseButton from '@/components/base/BaseButton.vue'
-import type { BaseButtonProps } from '../base/BaseButton.props.ts'
+import { computed, ref, useSlots } from "vue"
+import IconRenderer from "./IconRender.vue"
 
-const attrs = useAttrs()
-defineOptions({ inheritAttrs: false })
-defineProps<BaseButtonProps>()
+const props = defineProps({
+  icon: [String, Object],
+  text: String,
+  label: String,
+  img: Object,
+  attrs: Object,
+})
+
+const emit = defineEmits(["click"])
+const slots = useSlots()
+
+const iconRef = ref(null)
+const hasDefaultSlot = computed(() => !!slots.default)
+
+const hasContent = computed(() =>
+  !!(props.text || props.label || props.img || slots.default)
+)
+
+const isIconOnly = computed(() =>
+  !!props.icon && !hasContent.value
+)
+
+function handleClick(e: MouseEvent) {
+  emit("click", e)
+}
+
+function playLottieAnimation() {
+  iconRef.value?.play?.()
+}
+
+function pauseLottieAnimation() {
+  iconRef.value?.pause?.()
+}
 </script>
---
+++ src/components/ui/IconRender.vue
@@ -0,0 +1,76 @@
+<template>
+  <div v-if="isLottieUrl" ref="container" class="lottie-icon" />
+
+  <img v-else-if="isUrl" :src="icon" style="width: 1.5em; height: auto;" />
+
+  <span v-else-if="isEmoji" style="font-size: 1.2em;">
+    {{ icon }}
+  </span>
+
+  <v-icon v-else class="material-symbols-rounded">
+    {{ icon }}
+  </v-icon>
+</template>
+
+<script setup lang="ts">
+import { ref, computed, onMounted, onBeforeUnmount } from "vue"
+import lottie from "lottie-web"
+
+const props = defineProps({
+  icon: [String, Object]
+})
+
+const container = ref<HTMLElement | null>(null)
+let lottieInstance: any = null
+
+const isUrl = computed(() =>
+  typeof props.icon === "string" && props.icon.startsWith("http")
+)
+
+const isEmoji = computed(() =>
+  typeof props.icon === "string" && /\p{Emoji}/u.test(props.icon)
+)
+
+const isLottieUrl = computed(() =>
+  typeof props.icon === "string" && props.icon.endsWith(".json")
+)
+
+onMounted(() => {
+  if (isLottieUrl.value && container.value) {
+    lottieInstance = lottie.loadAnimation({
+      container: container.value,
+      renderer: "svg",
+      loop: true,
+      autoplay: false,
+      path: props.icon
+    })
+  }
+})
+
+onBeforeUnmount(() => {
+  if (lottieInstance) {
+    lottieInstance.destroy()
+    lottieInstance = null
+  }
+})
+
+function play() {
+  lottieInstance?.play()
+}
+
+function pause() {
+  lottieInstance?.pause()
+}
+
+defineExpose({ play, pause })
+</script>
+
+<style scoped>
+.lottie-icon {
+  width: 1.5em;
+  height: 1.5em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
