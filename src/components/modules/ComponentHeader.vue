4e2c0fb868b07be8973f6e1ebc09f166f0a02efa
Tui <146558116+arthuurtm@users.noreply.github.com>
2026-01-25 12:32:24 -0300

    refactor: simplifica o cabeçalho do componente e remove código não utilizado

--- src/components/modules/ComponentHeader.vue
+++ src/components/modules/ComponentHeader.vue
@@ -1,126 +1,38 @@
 <template>
-  <v-app-bar
-    v-if="!hidden"
-    border="b"
-    class="px-4"
-    elevation="0"
-    height="80"
-  >
-    <v-container class="d-flex align-center pa-0" style="max-width: 1280px">
+  <transition name="slide-top">
+    <v-app-bar v-if="!hidden" class="px-4" elevation="0" style="border-radius: 0 !important" scroll-behavior="hide">
+      <v-container class="d-flex align-center pa-0" style="max-width: 1280px">
+        <div class="d-flex align-center gap-4">
+          <logo style="cursor: pointer; height: 38px" @click="router.push({ name: 'Home' })" />
 
-      <div class="d-flex align-center gap-4">
-        <CLogo style="cursor: pointer" @click="router.push({ name: 'Home' })" />
+          <template v-if="title">
+            <span class="mx-4 text-grey">x</span>
+            <span class="text-h6 font-weight-bold">{{ title }}</span>
+          </template>
+        </div>
 
-        <template v-if="title">
-          <span class="mx-4 text-grey">x</span>
-          <span class="text-h6 font-weight-bold">{{ title }}</span>
-        </template>
-      </div>
+        <v-divider class="mx-4" inset vertical />
 
-      <v-divider class="mx-4" inset vertical />
+        <nav class="d-flex gap-4 justify-center align-center">
+          <slot name="left" />
+        </nav>
 
-      <nav class="d-flex gap-4">
-        <CButton
-          v-for="(btn, index) in finalNavLinks.left"
-          :key="index"
-          v-bind="btn"
-          @click="btn.action?.()"
-        />
-      </nav>
-
-      <v-spacer /> <div class="d-flex gap-6">
-        <CButton
-          v-for="(btn, index) in finalNavLinks.right"
-          :key="'right-' + index"
-          v-bind="btn"
-          @click="btn?.action"
-        />
-      </div>
-    </v-container>
-  </v-app-bar>
+        <v-spacer />
+        <div class="d-flex gap-6 justify-center align-center">
+          <slot name="right" />
+        </div>
+      </v-container>
+    </v-app-bar>
+  </transition>
 </template>
 
-<script setup>
-  import { computed } from 'vue'
-  import router from '@/router'
+<script setup lang="ts">
+import router from "@/router";
+import Logo from "#src/components/ui/Logo.vue";
 
-  // Props
-  const props = defineProps({
-    hidden: {
-      type: Boolean,
-      default: false,
-    },
-    navLinks: {
-      type: Object,
-      default: () => ({ left: [], right: [] }),
-    },
-    title: String,
-  })
-
-  const finalNavLinks = computed(() => {
-    return {
-      left: props.navLinks?.left,
-      right: [...(props.navLinks && Array.isArray(props.navLinks.right) ? props.navLinks.right : [])],
-    }
-  })
+// Props
+const props = defineProps<{
+  hidden?: Boolean;
+  title?: String;
+}>();
 </script>
-
-<style scoped>
-.header-container {
-  width: 100%;
-  background-color: var(--bg2);
-  position: relative;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  max-width: 1280px; /* Limita a largura para telas grandes, como a Steam faz */
-  margin: 0 auto;
-  height: 80px; /* Altura fixa para o header */
-  transition:
-    transform 0.3s ease,
-    visibility 0.3s ease;
-}
-
-.header-container.hidden .header {
-  visibility: hidden;
-  transform: translateY(-100%);
-}
-
-.header-info {
-  display: flex;
-  gap: 1rem;
-}
-
-.separator {
-  width: 1px;
-  height: -webkit-fill-available;
-  background: var(--border);
-  margin: 0 1rem;
-}
-
-/* --- SEÇÃO ESQUERDA --- */
-.header-left {
-  display: flex;
-  align-items: center;
-  gap: 2rem;
-}
-
-/* --- NAVEGAÇÃO PRINCIPAL --- */
-.main-nav {
-  display: flex;
-  align-items: center;
-  gap: 1.5rem;
-  height: 100%;
-}
-
-/* --- SEÇÃO DIREITA --- */
-.header-right {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-}
-</style>
