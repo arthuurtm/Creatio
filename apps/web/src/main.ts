bf9ca3a1d36f73a2c409ec8192bf7d59e7d34c96
Tui <146558116+arthuurtm@users.noreply.github.com>
2026-02-07 01:43:11 -0300

    refactor: move componentes ainda usados para /styles e adiciona a importação no main.ts

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -6,15 +6,16 @@
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
-import "@/assets/css/main.css";
+// import "@/assets/css/main.css";
 import "@vue-flow/core/dist/style.css";
 import "@vue-flow/core/dist/theme-default.css";
 // import Antd from "ant-design-vue";
 import { createApp, type App as VueApp } from "vue";
+import App from "./App.vue";
 // import Vue3Toasity, { type ToastContainerOptions } from "vue3-toastify";
 import { registerPlugins } from "./plugins";
-import App from "./App.vue";
 import pinia from "./plugins/pinia";
+import "@/styles/global.scss";
 
 // function resolveGLobalComponents(instance: VueApp<Element>) {
 // 	instance.use(Antd);
