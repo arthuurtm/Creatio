d3e769ae39e8309ecd6289092b988c0b5a2c76c3
Tui <146558116+arthuurtm@users.noreply.github.com>
2026-02-28 13:14:59 -0300

    style: remove estilos desnecessários

--- apps/web/src/components/ui/Logo.vue
+++ apps/web/src/components/ui/Logo.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
-import Logo from '@/assets/logo.svg'
+import LogoSVG from '@/assets/logo.svg'
 </script>
 
 <template>
-  <Logo />
+  <LogoSVG class="logo"/>
 </template>
 
 
 <style scoped>
 .logo {
   fill: black;
-  background: #4e8cff;
-  padding: 8px;
-  border-radius: 12px;
 }
 </style>
